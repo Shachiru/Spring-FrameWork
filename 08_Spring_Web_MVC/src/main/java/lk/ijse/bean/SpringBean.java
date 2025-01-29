2dd988425ad1146bb940a2c01057b8100b30e744
Shachiru <shachirurashmika35@gmail.com>
2025-01-29 15:06:02 +0530

    webApp create using bean

---
+++ 08_Spring_Web_MVC/src/main/java/lk/ijse/bean/SpringBean.java
@@ -0,0 +1,39 @@
+package lk.ijse.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    public SpringBean() {
+        System.out.println("SpringBean Constructor");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("SpringBean Bean Factory");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("SpringBean Bean Name Set " + name);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("SpringBean Object Destroyed");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("SpringBean Initializing Bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("SpringBean Application Context");
+    }
+}
---
+++ 08_Spring_Web_MVC/src/main/java/lk/ijse/controller/HelloController.java
@@ -0,0 +1,18 @@
+package lk.ijse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+    public HelloController() {
+        System.out.println("Hello controller");
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
