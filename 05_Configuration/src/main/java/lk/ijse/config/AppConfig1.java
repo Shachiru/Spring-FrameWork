946b9d6f7e3c35a47f230a9612740339aae666ef
Shachiru <shachirurashmika35@gmail.com>
2025-01-29 11:17:28 +0530

    multiple configuration

--- 05_Configuration/src/main/java/lk/ijse/config/AppConfig1.java
+++ 05_Configuration/src/main/java/lk/ijse/config/AppConfig1.java
@@ -1,5 +1,6 @@
 package lk.ijse.config;
 
+import lk.ijse.bean.SpringBean;
 import lk.ijse.bean.SpringBean1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig1 {
     @Bean
-    public SpringBean1 springBean1() {
-        return new SpringBean1();
+    public SpringBean springBean() {
+        return new SpringBean();
     }
 }
--- 05_Configuration/src/main/java/lk/ijse/config/AppConfig2.java
+++ 05_Configuration/src/main/java/lk/ijse/config/AppConfig2.java
@@ -1,15 +1,14 @@
 package lk.ijse.config;
 
-import lk.ijse.bean.SpringBean;
 import lk.ijse.bean.SpringBean1;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-//@ComponentScan(basePackages = "lk.ijse.bean")
-@Import({AppConfig1.class, AppConfig2.class})
-public class AppConfig {
-
+public class AppConfig2 {
+    @Bean
+    public SpringBean1 springBean1() {
+        return new SpringBean1();
+    }
 }
